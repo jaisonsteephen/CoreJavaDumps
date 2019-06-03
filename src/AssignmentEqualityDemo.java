2574a0cdead528996550b957bb2ae038a244f315
jaison steephen <jaisonsteeephen@gmail.com>
2019-06-03 23:25:22 +0530

    AssignmentEqualityDemo

--- src/AssignmentEqualityDemo.java
+++ src/AssignmentEqualityDemo.java
@@ -1,14 +1,26 @@
 
 public class AssignmentEqualityDemo {
 public static void main(String args[]){
-	Long l=1l;
-	Long l2=l;
-	l=3l;
-	System.out.println(l2);
-	
-	StringBuffer sb= new StringBuffer("hai");
-	StringBuffer sb2=sb;
-	sb.append("h");
-	System.out.println(sb2);
+
+/**
+ * 
+ *     "abc"==new String("abc")			:false
+ *	   "abc"=new String("abc").intern()	:true
+ *     "abc".equals(new String("abc"))	:true
+ * 
+ * 
+ */
+
+String x="abc";
+String y=new String("abc");
+String z=new String("abc").intern();
+
+System.out.println("\"abc\"==new String(\"abc\")	 :"+Boolean.toString(x==y));
+System.out.println("\"abc\"=new String(\"abc\").intern() :"+Boolean.toString(x==z));
+
+System.out.println("\"abc\".equals(new String(\"abc\"))	 :"+ Boolean.toString(x.equals(y)));
+
+
+
 }
 }
