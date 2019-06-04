298f39d056a05b7077af8f6528f33bf2c874e31b
jaison steephen <jaisonsteeephen@gmail.com>
2019-06-04 12:51:44 +0530

    SingletonDemoDoubleCheck

--- src/com/pattern/singleton/SingletonDemoDoubleCheck.java
+++ src/com/pattern/singleton/SingletonDemoDoubleCheck.java
@@ -1,35 +1,47 @@
 package com.pattern.singleton;
-
-class SingletonLogger {
+/**
+ * 
+ * @author jaison
+ * Here we are using SingletonClz to access LoggerClass.
+ * LoggerClass will not be accessible to outside world, since we are not providing the lib that contain LoggerClass to them.
+ *
+ *  output
+ *  -----------
+ *  true
+ *  Hello World
+ */
+class LoggerClass {
 	
-	//***********Singleton functionality Block **********************
-	private SingletonLogger() {//	private constructor is necessary. 
-							   //	otherwise default constructor will be accessible
-		
+	public void logMyMsg(String msg) {
+		System.out.println(msg);
 	}
-	private static SingletonLogger instance = null;
-	public static SingletonLogger getSingleton() {
+	
+}
+
+class SingletonClz  extends LoggerClass{
+	
+	private SingletonClz() {
+		// private constructor is necessary.
+		// otherwise default constructor will be accessible to outside world i.e. SingletonDemoDoubleCheck 
+	}
+	private static SingletonClz instance = null;
+	public static SingletonClz getSingleton() {
 		if (instance == null) {
-			synchronized (SingletonLogger.class) {
+			synchronized (SingletonClz.class) {
 				if (instance == null) {
-					instance = new SingletonLogger();
+					instance = new SingletonClz();
 				}
 			}
 		}
 		return instance;
 	}
-
-	//************Functionality apart from Singleton********************
-	public void logMyMsg(String msg) {
-		System.out.println(msg);
-	}
-
 }
 
 public class SingletonDemoDoubleCheck {
 	public static void main(String args[]) {
-		SingletonLogger logger = SingletonLogger.getSingleton();
-		SingletonLogger logger2 = SingletonLogger.getSingleton();
+		LoggerClass c=new LoggerClass();
+		SingletonClz logger = SingletonClz.getSingleton();
+		SingletonClz logger2 = SingletonClz.getSingleton();
 		//SingletonLogger logger3=new SingletonLogger();  //Not possible due to private constructor
 		System.out.println(logger==logger2);
 		logger.logMyMsg("Hello World");
